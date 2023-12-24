02131c007a091a9e96d76296398eb7e7bb4a4712
LinHong <linlin.hong1@gmail.com>
2023-12-24 15:16:24 -0500

    anchorButton

--- src/app/components/Buttons.tsx
+++ src/app/components/Buttons.tsx
@@ -1,17 +1,87 @@
 import React from "react"
+import { useState } from "react"
+import { motion } from "framer-motion"
 
 type ButtonProps = {
   link : string;
   content : string;
+  contentStart : string;
+  contentEnd : string;
 }
 
 
-export const AnchorButton : React.FC<ButtonProps> = ({link, content}) => {
+export const AnchorButton : React.FC<ButtonProps> = ({link, contentStart, contentEnd}) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const strokeWidth = 2.2;
+
+  const svgVariants = {
+    hidden: { width: 0, height: 30 },
+    visible: { width: 5, height: 30, transition: { duration: 0.3 } },
+  };
+
+  const lineVariants = {
+    hidden: { scaleY: 0, originY: 0 },
+    visible: { scaleY: 1, originY: 0, transition: { delay: 0.4, duration: 0.15 } },
+  };
+
+  const arrowHeadVariants = {
+    hidden: { scaleY: 0, },
+    visible: { scaleY: 1, transition: { delay: 0.5, duration: 0.2 }},
+  }
+
+  
   return (
-    <button>
-        <a href={link}>{content}</a>
+    <button
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      className="flex items-center justify-around border w-fit"
+    >
+      <a href={link} className={`flex flex-row gap-0 h-8 items-center justify-center font-bold duration-150 ${isHovered? "opacity-100 ": "opacity-50"}`}>
+        {contentStart}
+        <motion.svg
+          initial="hidden"
+          animate={isHovered ? "visible" : "hidden"}
+          variants={svgVariants}
+          viewBox="0 0 5 30"
+          overflow="visible"
+          className="flex items-start"
+        >
+          <motion.line // vertical line
+            x1="2.5"
+            y1="0"
+            x2="2.5"
+            y2="30"
+            stroke="#F6F6F8"
+            strokeWidth= {strokeWidth}
+            strokeLinecap="round"
+            variants={lineVariants}
+          />
+          <motion.line
+            x1="-3"
+            y1="25.5"
+            x2="2.5"
+            y2="30.2"
+            stroke="#F6F6F8"
+            strokeWidth= {strokeWidth}
+            strokeLinecap="round"
+            variants={arrowHeadVariants}
+          />
+          <motion.line
+            x1="8"
+            y1="25.5"
+            x2="2.5"
+            y2="30.2"
+            stroke="#F6F6F8"
+            strokeWidth= {strokeWidth}
+            strokeLinecap="round"
+            variants={arrowHeadVariants}
+          />
+        </motion.svg>
+        {contentEnd}
+      </a>
     </button>
-  )
+  );
 }
 
 export const SolidButton : React.FC<ButtonProps> = ({link, content}) => {
--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -1,10 +1,11 @@
+'use client'
 import React from "react";
 import { AnchorButton, SolidButton } from "../components/Buttons";
 
 
 const Hero = () => {
   return (
-    <div className="border flex flex-col gap-16 w-full h-screen px-16">
+    <div className="flex flex-col gap-16 w-full h-screen px-16">
       <h1 className="text-3xl">Hi, I'm Lin,</h1>
       <h1 className="text-3xl">a Software Engineer</h1>
       <p>
@@ -12,8 +13,7 @@ const Hero = () => {
         coding experiences and made 16+ projects.
       </p>
 
-      <AnchorButton link="hi" content="HELLO"></AnchorButton>
-      <SolidButton link="hi" content="HELLO"></SolidButton>
+      <AnchorButton link="" contentStart="PROJ" contentEnd="ECTS" content={""}></AnchorButton>
     </div>
   );
 };
